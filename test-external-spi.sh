1c657fc4befc8e94be4ae9cd9962013c71322b62
Jason Klas <jklas.alcor@gmail.com>
2022-11-23 18:42:18 +0000

    Export SPI CS GPIO pin if not already exported, don't use bc for float division formatting, minor cleanup

--- test-external-spi.sh
+++ test-external-spi.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 set -u
 
-if [ ! -d /sys/class/gpio/gpio62/ ]; then
-  echo "Run /home/`whoami`/start_server.sh script then kill it to expose external SPI GPIO pins"
-  exit 1
-fi;
+EXTERNAL_SPI_INTERFACE='/dev/spidev2.0'
+EXTERNAL_SPI_CS_PIN='/sys/class/gpio/gpio62'
+
+if [[ ! -e ${EXTERNAL_SPI_INTERFACE} ]]; then 
+	echo "ERROR: ${EXTERNAL_SPI_INTERFACE} not found!  Check DTBs installed to /dev/mmcblk2p1"
+	exit 1
+fi
+
+if [[ ! -e ${EXTERNAL_SPI_CS_PIN} ]]; then
+	echo "ERROR: ${EXTERNAL_SPI_CS_PIN} not found! Exporting GPIO pin 62 and setting pin 62 direction to out."
+	echo 62 > /sys/class/gpio/export
+	# set direction of GPIO pin
+	echo 'out' > ${EXTERNAL_SPI_CS_PIN}/direction
+
+	echo 'Please re-run script.'
+	exit 1
+fi
 
 echo '# Testing integrity of external SPI communications with MCU by reading sw_version SPI table register'
 TOTAL_READ_ATTEMPTS=0
@@ -17,16 +30,16 @@ while [ true ]; do
 	while [ "${VALID_SW_VERSION}" != '' ]; do
 		TOTAL_READ_ATTEMPTS=$((TOTAL_READ_ATTEMPTS+1))
 		READ_ATTEMPTS=$((READ_ATTEMPTS+1))
-		echo 0 > /sys/class/gpio/gpio62/value
-		SPIDEV_TEST_OUTPUT=`./spidev_test -D /dev/spidev2.0 -b 8 -p "R\x0c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" -v -s ${SPI_CLOCK}`
-		echo 1 > /sys/class/gpio/gpio62/value
-		VALID_SW_VERSION=`echo $SPIDEV_TEST_OUTPUT | grep ${EXPECTED_SW_VERSION}`
+		echo 0 > ${EXTERNAL_SPI_CS_PIN}/value
+		SPIDEV_TEST_OUTPUT=`./spidev_test -D ${EXTERNAL_SPI_INTERFACE} -b 8 -p "R\x0c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" -v -s ${SPI_CLOCK}`
+		echo 1 > ${EXTERNAL_SPI_CS_PIN}/value
+		VALID_SW_VERSION=`echo ${SPIDEV_TEST_OUTPUT} | grep ${EXPECTED_SW_VERSION}`
 		
 		if [ `expr $TOTAL_READ_ATTEMPTS % 1000` = 0 ]; then
 			echo "external SPI clock: ${SPI_CLOCK} hz"
 			echo "Total sw_version register read attempts: ${TOTAL_READ_ATTEMPTS}"
 			echo "Failures to correctly read sw_version register: ${SPI_READ_FAILURES}"
-			echo "Failure rate: `echo ${SPI_READ_FAILURES}/${TOTAL_READ_ATTEMPTS} | bc -l`"
+			printf "Failure rate: %.4f\n" $((10**4 * ${SPI_READ_FAILURES}/${TOTAL_READ_ATTEMPTS}))e-4
 		fi
 		
 		if [ $TOTAL_READ_ATTEMPTS = 10000 ]; then
